31f77547db94754b8df5e153f3271a053738c876
Ivan <sir.sibiliv@gmail.com>
2025-05-20 01:19:31 +0300

    aw3t_2.5

--- src/App.vue
+++ src/App.vue
@@ -16,32 +16,28 @@
       <v-divider></v-divider>
 
       <v-list dense>
-        <v-list-item>
+
+        <v-list-item v-for="link in links" :key="link.title">
           <template v-slot:prepend>
-            <v-icon icon="mdi-cake-variant"></v-icon>
+            <v-icon :icon="link.icon"></v-icon>
           </template>
-          <v-list-item-title>Link One</v-list-item-title>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
+
     </v-navigation-drawer>
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn>
-          <v-icon
-              start
-              icon="mdi-cake-variant"
-          ></v-icon>
-          Link One
+
+        <v-btn v-for="link in links" :key="link.title"><v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
         </v-btn>
-        <v-btn>Link Two</v-btn>
-        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
+    <v-main>
+    </v-main>
   </v-app>
 </template>
 
@@ -49,7 +45,14 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        {title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders"},
+        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
+      ]
     }
   }
 }
