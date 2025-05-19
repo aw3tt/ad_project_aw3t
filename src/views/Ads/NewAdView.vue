68d91c79dafea26f4208a5549d4fde6cf689a208
Ivan <sir.sibiliv@gmail.com>
2025-05-20 01:30:39 +0300

    aw3t_2.7

---
+++ src/views/Ads/NewAdView.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <h1>New Ad view</h1>
+    </v-row></v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
