03e377614061174bb967a2238cb199c275fd3340
Ivan <sir.sibiliv@gmail.com>
2025-05-20 01:12:50 +0300

    aw3t_2.2

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn  text>Link One</v-btn>
-        <v-btn  text>Link Two</v-btn>
-        <v-btn  text>Link Three</v-btn>
+        <v-btn>
+          <v-icon
+              start
+              icon="mdi-cake-variant"
+          ></v-icon>
+          Link One
+        </v-btn>
+        <v-btn>Link Two</v-btn>
+        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
     <v-main>
