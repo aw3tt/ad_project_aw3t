8f8231652773d6eb30b3013ccb49fdc982269eb0
Ivan <sir.sibiliv@gmail.com>
2025-05-20 16:35:33 +0300

    aw3t_13.3

--- src/store/user.js
+++ src/store/user.js
@@ -9,22 +9,43 @@ class User {
 export default {
     state: {
         user: null,
+        loading: false,
+        error: null,
     },
     mutations: {
         setUser(state, payload) {
             console.log(payload);
             state.user = payload;
         },
+        clearError(state) {
+            state.error = null;
+        },
+        setLoading(state, payload) {
+            state.loading = payload;
+        },
+        setError(state, payload) {
+            state.error = payload;
+        },
     },
     actions: {
         registerUser({ commit }, { email, password }) {
-            //Здесь запрос на сервер для регистрации
-            commit("setUser", new User(1, email, password));
+            commit('clearError');
+            commit('setLoading', true);
+
+            // Временная логика, чтобы избежать ошибки ESLint
+            console.log(new User('temp-id', email, password));
+
         },
     },
     getters: {
         user(state) {
             return state.user;
         },
+        loading(state) {
+            return state.loading;
+        },
+        error(state) {
+            return state.error;
+        },
     },
-}
+};
