2d061735fb20f50458a4468ee8813daf62b00b4d
Ivan <sir.sibiliv@gmail.com>
2025-05-20 17:11:54 +0300

    aw3t_17.2

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -3,7 +3,7 @@
     <v-row justify="center">
       <v-col cols="12">
         <v-card class="mt-5">
-          <v-img height="400px" :src="ad.src" cover></v-img>
+          <v-img height="300px" :src="ad.src"></v-img>
           <v-card-text>
             <h1 class="text--primary mb-3">{{ ad.title }}</h1>
             <p>
@@ -13,7 +13,7 @@
 
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="warning" color="orange">Edit</v-btn>
+            <modal-dialog></modal-dialog>
             <v-btn class="success" color="green">Buy</v-btn>
           </v-card-actions>
         </v-card>
@@ -24,16 +24,18 @@
 </template>
 
 <script>
+import EditAdModal from './EditAdModal.vue';
+
 export default {
-  data() {
-    return {};
-  },
   props: ['id'],
   computed: {
     ad() {
-      const id = this.id
-      return this.$store.getters.adById(id)
-    }
-  }
+      const id = this.id;
+      return this.$store.getters.adById(id); // Получаем объявление по id
+    },
+  },
+  components: {
+    'modal-dialog':EditAdModal, // Регистрируем компонент
+  },
 }
 </script>
