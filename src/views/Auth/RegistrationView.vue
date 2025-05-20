777abec7a0c9dd608b3df930e7e4c7ddc93f5d9c
Ivan <sir.sibiliv@gmail.com>
2025-05-20 14:58:22 +0300

    aw3t_4.2

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -9,15 +9,16 @@
           </v-toolbar>
           <v-card-text>
             <v-form v-model="valid" ref="form" validation>
-              <v-text-field prepend-icon="mdi-account" name="confirm-password" label="Confirm Password" type="password" v-model="confirmPassword" :rules="confirmPasswordRules"></v-text-field>
+              <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email" v-model="email" :rules="emailRules"></v-text-field>
               <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password" v-model="password" :rules="passwordRules"></v-text-field>
+              <v-text-field prepend-icon="mdi-lock" name="confirm-password" label="Confirm Password" type="password" v-model="confirmPassword" :rules="confirmPasswordRules"></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
-
             <v-spacer></v-spacer>
-
-            <v-btn color="primary" @click="onSubmit" :disabled="!valid">Create Account</v-btn>
+            <v-btn color="primary" @click="onSubmit" :disabled="!valid">
+              Create Account
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -31,14 +32,19 @@ export default {
     return {
       email: "",
       password: "",
+      confirmPassword: "",
       valid: false,
       emailRules: [
         v => !!v || 'E-mail is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
       ],
       passwordRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => v === this.password || 'Password should match'
       ]
     }
   },
