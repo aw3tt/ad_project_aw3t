d86f8f7943c4fba7c4af4e7cd97313b5e8d38142
Ivan <sir.sibiliv@gmail.com>
2025-05-20 16:43:00 +0300

    aw3t_14.3

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -16,7 +16,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click="onSubmit" :disabled="!valid">
+            <v-btn color="primary" @click="onSubmit" :loading="loading" :disabled="!valid">
               Login
             </v-btn>
           </v-card-actions>
@@ -42,6 +42,12 @@ export default {
         v => !!v || 'Name is required',
         v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
       ],
+    }
+  },
+
+  computed: {
+    loading() {
+      return this.$store.getters.loading
     }
   },
 
