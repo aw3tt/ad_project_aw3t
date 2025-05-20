52c99c553df9729d0581cce7f9e4761fec70253d
Ivan <sir.sibiliv@gmail.com>
2025-05-20 15:09:25 +0300

    aw3t_5.5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,23 +16,22 @@
       </v-col>
     </v-row>
   </v-container>
-  <v-container>
+  <v-container grid-list-lg>
     <v-row justify="center">
-      <v-col cols="12" xs="12">
+      <v-col cols="3" sm="3" md="3" lg="3" xs="12" v-for="ad in ads" :key="ad.id">
+
         <v-card>
-          <v-img height="200px"></v-img>
+          <v-img :src="ad.src" height="200px"></v-img>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb0">Здесь будет заголовок</h3>
-              <div>Здесь будет описание</div>
+              <h3 class="headline mb0">{{ ad.title }}</h3>
+              <div>{{ ad.desc }}</div>
             </div>
           </v-card-title>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn text>Open</v-btn>
-            <v-btn raised color="primary">
-              Buy
-            </v-btn>
+            <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+            <v-btn raised color="primary">Buy</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -81,11 +80,11 @@ export default {
 
 <style scoped>
   .ad-link {
-    position:absolute;
+    position: absolute;
     bottom: 50px;
-    left:50%;
-    background: rgb(0,0,0,0.5);
-    transform: translate(-50%,0);
+    left: 50%;
+    background: rgb(0, 0, 0, 0.5);
+    transform: translate(-50%, 0);
     padding: 5px 15px;
     border-top-right-radius: 5px;
     border-top-left-radius: 5px;
