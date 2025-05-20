177810ffc33248372cca4e5f47d8e27b647e5a49
Ivan <sir.sibiliv@gmail.com>
2025-05-20 16:39:35 +0300

    aw3t_13.5

--- src/store/user.js
+++ src/store/user.js
@@ -28,9 +28,29 @@ export default {
         },
     },
     actions: {
-        registerUser({ commit }, { email, password }) {
+        async registerUser({ commit }, { email, password }) {
             commit('clearError');
             commit('setLoading', true);
+
+            //Здесь выполняется запрос на сервер
+            let isRequestOk = false
+            let promise = new Promise(function(resolve) {
+                setTimeout(() => resolve('Done'),3000);
+            });
+
+            if (isRequestOk) {
+                await promise.then(()=> {
+                    commit('setUser', new User(1, email, password))
+                    commit('setLoading', false)
+                })
+            } else {
+                await promise.then(()=> {
+                    commit('setLoading', false)
+                    commit('setError', 'Ошибка регистрации')
+                    throw 'Упс... Ошибка регистрации'
+                })
+            }
+
 
             // Временная логика, чтобы избежать ошибки ESLint
             console.log(new User('temp-id', email, password));
