d430457e48e31bceafdcfd3e76a201c10824bd7b
Ivan <sir.sibiliv@gmail.com>
2025-05-20 15:16:44 +0300

    aw3t_7.2

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -48,6 +48,22 @@
 export default {
   data() {
     return {
+      valid: false,
+      title: "",
+      description: "",
+      promo: true
+    }
+  },
+  methods: {
+    createAd() {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          desc: this.description,
+          promo: this.promo
+        }
+        console.log(ad)
+      }
     }
   }
 }
